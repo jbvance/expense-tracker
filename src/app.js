8422af8dccb1540919f9faba5ab3cb51edbfd83c
Jason Vance <jbvance@gmail.com>
2018-01-24 13:08:32 -0600

    add react router and test routes and components

--- src/app.js
+++ src/app.js
@@ -1,6 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<p>Initial Content</p>, document.getElementById('app'));
+const ExpenseDashboardPage = () => (
+    <div>
+        This is from my dashboard
+    </div>
+);
+
+const AddExpensePage = () => (
+    <div>
+        This is from my add expense component
+    </div>
+);
+
+const EditExpensePage = () => (
+    <div>
+        This is from my edit expense component
+    </div>
+);
+
+const HelpPage = () => (
+    <div>
+        This is from my Help component
+    </div>
+);
+
+const NotFoundPage = () => (
+    <div>
+        404 NOT FOUND
+    </div>
+);
+
+const routes = (
+    <BrowserRouter>
+        <Switch>
+            <Route exact={true} path="/" component={ExpenseDashboardPage} />
+            <Route exact={true} path="/create" component={AddExpensePage} />
+            <Route exact={true} path="/edit" component={EditExpensePage} />
+            <Route exact={true} path="/help" component={HelpPage} />
+            <Route component={NotFoundPage} />
+        </Switch>
+    </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,8 @@ module.exports = {
   },
   devtool: 'cheap-module-eval-source-map',
   devServer: {
-    contentBase: path.join(__dirname, 'public')
+    contentBase: path.join(__dirname, 'public'),
+    //Tell dev-server we're handling router via client-side
+    historyApiFallback: true
   }
 };
