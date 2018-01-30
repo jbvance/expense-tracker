0ae664fd00639f9663b563c36c9bbdb7cf400aa5
Jason Vance <jbvance@gmail.com>
2018-01-30 09:02:06 -0600

    setup enzyme for testing

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,6 @@ const Header = () => (
       <h1>Expense Tracker</h1>
       <NavLink exact={true} to="/" activeClassName="is-active">Dashboard</NavLink>
       <NavLink exact={true} to="/create" activeClassName="is-active">Create Expense</NavLink>
-      <NavLink exact={true} to="/help" activeClassName="is-active">Help</NavLink>
   </header>
 );
 
