74624e3f09f018635cc40aa18ba7127144dd9cc0
Jason Vance <jbvance@gmail.com>
2018-01-30 10:15:41 -0600

    add tests for ExpenseDashboardPage, NotFoundPage, ExpenseList, and ExpenseListItem

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -3,13 +3,22 @@ import { connect } from 'react-redux';
 import ExpenseListItem from './ExpenseListItem';
 import selectExpenses from '../selectors/expenses';
 
-const ExpenseList = (props) => (
+// Use as named export so you can import for testing
+// without state being passed in
+export const ExpenseList = (props) => (
   <div>
-    <h1>Expense List </h1>    
-    {props.expenses.map((expense) => {
-      return <ExpenseListItem key={expense.id} {...expense}
-             />
-    })}
+    {
+      props.expenses.length === 0 ? (
+        <p>No expenses</p>
+      ) : (
+
+          props.expenses.map((expense) => {
+            return <ExpenseListItem key={expense.id} {...expense}
+            />
+          })
+
+        )
+    }
   </div>
 );
 
--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -4,9 +4,5 @@ import Header from '../../components/Header';
 
 test('should render Header correctly', () => {
   const wrapper = shallow(<Header />);
-  expect(wrapper).toMatchSnapshot();
-  //expect(wrapper.find('h1').text()).toBe('Expense Tracker');
-  // const renderer = new ReactShallowRenderer();
-  // renderer.render(<Header />)
-  // expect(renderer.getRenderOutput()).toMatchSnapshot();
+  expect(wrapper).toMatchSnapshot();  
 });
