d91d8f5b008e55bd373267df149b6aeb7e28935f
Jason Vance <jbvance@gmail.com>
2018-02-02 10:25:25 -0600

    set up production build and server

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+
+// Match all unmatched routes to index.html. Otherwise, when
+// a refuser refreshes browser, a 404 will occur
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log('Server is up!');
+});
