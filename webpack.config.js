30dd87c6275d6242b53648afdbb173c451fa1654
Jason Vance <jbvance@gmail.com>
2018-02-02 08:52:35 -0600

    Setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,54 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-  entry: './src/app.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [{
-      loader: 'babel-loader',
-      test: /\.js$/,
-      exclude: /node_modules/
-    }, {
-      test: /\.s?css$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader'
-      ]
-    }]
-  },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    //Tell dev-server we're handling router via client-side
-    historyApiFallback: true
-  }
-};
+module.exports = (env) => {
+  const isProduction = env === 'production';
+  const CSSExtract = new ExtractTextPlugin('styles.css');
+
+  console.log('env', env);
+  return {   
+      entry: './src/app.js',
+      output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+      },
+      module: {
+        rules: [{
+          loader: 'babel-loader',
+          test: /\.js$/,
+          exclude: /node_modules/
+        }, {
+          test: /\.s?css$/,
+          use: CSSExtract.extract({
+            use: [
+              {
+                loader: 'css-loader',
+                options: {
+                  sourceMap: true
+                }
+              },
+              {
+                loader: 'sass-loader',
+                options: {
+                  sourceMap: true
+                }
+              }
+            ]
+          })
+        }]
+      },
+      plugins: [
+        CSSExtract
+      ],
+      // source-map will only load whend dev tools are opened in browser
+      // inline-source-map is larger and is not suitable for production
+      devtool: isProduction ? 'source-map' : 'inline-source-map',
+      devServer: {
+        contentBase: path.join(__dirname, 'public'),
+        //Tell dev-server we're handling router via client-side
+        historyApiFallback: true
+      }    
+  };
+}
+
+
