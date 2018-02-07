8395290d12f77d7d50a009b18973d1e95bb42555
Jason Vance <jbvance@gmail.com>
2018-02-07 13:17:06 -0600

    Add PublicRoute component

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ import { startLogout } from '../actions/auth';
 export const Header = ({ startLogout }) => (
   <header>
       <h1>Expense Tracker</h1>
-      <NavLink exact={true} to="/" activeClassName="is-active">Dashboard</NavLink>
+      <NavLink to="/dashboard" activeClassName="is-active">Dashboard</NavLink>
       <NavLink exact={true} to="/create" activeClassName="is-active">Create Expense</NavLink>
       <button onClick={startLogout}>Logout</button>
   </header>
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -4,10 +4,10 @@ import createHistory from 'history/createBrowserHistory';
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
 import AddExpensePage from '../components/AddExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
-import HelpPage from '../components/HelpPage'
 import NotFoundPage from '../components/NotFoundPage';
 import LoginPage from '../components/LoginPage';
 import PrivateRoute from './PrivateRoute';
+import PublicRoute from './PublicRoute';
 
 //use history here rather so it can be used outside the context
 // of a component. For example, it's imported in app.js
@@ -17,11 +17,10 @@ const AppRouter = () => (
   <Router history={history}>
      <div>
           <Switch>
-            <Route exact={true} path="/" component={LoginPage} />
+            <PublicRoute exact={true} path="/" component={LoginPage} />
             <PrivateRoute exact={true} path="/dashboard" component={ExpenseDashboardPage} />
             <PrivateRoute exact={true} path="/create" component={AddExpensePage} />
             <PrivateRoute exact={true} path="/edit/:id" component={EditExpensePage} />
-            <Route exact={true} path="/help" component={HelpPage} />
             <Route component={NotFoundPage} />
           </Switch>
       </div>
